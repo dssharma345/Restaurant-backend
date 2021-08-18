66486f12c916c87541b9a26f34ad3a4683f83725
Deepak Sharma <dssharma345@gmail.com>
2021-08-18 21:22:47 +0530

    passport

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const bodyParser = require('body-parser');
 var User = require('../models/user');
+var passport = require('passport');
 
 var router = express.Router();
 router.use(bodyParser.json());
@@ -11,26 +12,28 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/signup', function(req, res, next){
-  User.findOne({username: req.body.username})
-    .then((user)=>{
-      if(user != null){
-        var err = new Error('User ' + req.body.username + ' already registered!');
-        err.status = 403;
-        next(err);
-      } else{
-          return User.create({username: req.body.username, password: req.body.password})
+  User.register(new User ({username: req.body.username}), 
+    req.body.password, (err, user)=>{
+      if(err){
+        res.statusCode = 500;
+        res.setHeader("Content-Type", "application/json");
+        res.json({err: err});
+      } 
+      else{
+        passport.authenticate('local')(req, res, ()=>{
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({success: true, status: 'Registration Successful'});
+        });
       }
-    })
-    .then((user)=>{
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({status: 'Registration Successful', user: user});
-    }, err=>next(err))
-    .catch(err=>next(err))
-})
+  });
+});
 
-router.post('/login', (req, res, next) => {
-  if(!req.session.user){
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json({success: true, status: "You are successfully logged in!"})
+  /* if(!req.session.user){
     var authHeader = req.headers.authorization;
     if (!authHeader) {
         var err = new Error('You are not authenticated!');
@@ -64,12 +67,12 @@ router.post('/login', (req, res, next) => {
         }
       })
       .catch((err)=>next(err));
-  }
-  else{
+  } */
+  /* else{
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end("You are already authenticated!");
-  }
+  } */
 });
 
 router.get('/logout', (req, res)=>{
